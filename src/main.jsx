621595bfff239129764e510c3cd9ea068eafdf86
wallissonghost-code <wallissonghost@gmail.com>
2026-08-05 09:54:24 -0300

    Load Neku v4 casino interface

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './casino-v4.css';
+import './neku-v4.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
