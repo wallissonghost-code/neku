0e08010dd2ac0aecf4be4ea5953c4a5880be7a51
wallissonghost-code <wallissonghost@gmail.com>
2026-08-05 11:10:00 -0300

    Load Neku v6 responsive interface

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './neku-v5.css';
+import './neku-v6.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
