ccf9d85d4087db69c228b5639cdb616d88d1b497
wallissonghost-code <wallissonghost@gmail.com>
2026-08-05 11:15:11 -0300

    Load mobile crash graph and cash controls fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './neku-v6.css';
+import './crash-mobile-v7.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
